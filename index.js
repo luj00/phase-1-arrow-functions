e8568d2ed85f197fd21f989ae2a4f446e9c7b1b1
root <root@DESKTOP-598GE48.localdomain>
2023-10-05 15:22:35 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+// Define divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Define square arrow function
+  const square = (x) => x * x;
+  
+  // Define add arrow function
+  const add = (a, b) => a + b;
+  
